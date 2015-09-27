098c476abc24d1b63fb67852e54b6fd730909204
zh423328 <zh423328@163.com>
2015-09-27 21:33:32 +0800

    md5取值工具
    暂时用的是同步方向，下次开来一条线程来进行取值

---
+++ Md5Tools/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Md5Tools
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(420, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "md5";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "文件夹";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(74, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(316, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(38, 79);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(457, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(38, 125);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(457, 148);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 311);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "MD5取值";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Md5Tools/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Md5Tools
+{
+    public partial class Form1 : Form
+    {
+        public List<string> list = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //按钮
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                //打开对话框
+                string strPath = dlg.SelectedPath;
+                textBox1.Text = strPath + "\\";    //对话框
+                string basePath = textBox1.Text;
+                list.Clear();
+
+                string filetxt = basePath + "files.txt";
+
+                File.Delete(filetxt);
+                //打开文件
+                GetFileList(strPath);
+
+                FileStream stream = File.Open(basePath + "files.txt", FileMode.CreateNew);
+                progressBar1.Value = 0;
+                progressBar1.Minimum = 0;
+                progressBar1.Maximum = list.Count;
+
+                for (int i = 0; i < list.Count; ++i )
+                {
+                    string filepath = list[i];
+
+                    string md5value = GetFileMd5(filepath);
+                    string strFileName = filepath.Replace(basePath, "");
+                    string value = strFileName + "|" + md5value + "\r\n";
+                    byte[] bytes = Encoding.Default.GetBytes(value);
+                    //获取值的md5
+                    stream.Write(bytes, 0, bytes.Length);
+                    progressBar1.Value++;
+                    bool scroll = false;
+                    if (this.listBox1.TopIndex == this.listBox1.Items.Count - (int)(this.listBox1.Height / this.listBox1.ItemHeight))
+                       scroll = true;
+                    this.listBox1.Items.Add(strFileName + "........(" + progressBar1.Value + "/" + progressBar1.Maximum + ")");
+                   if (scroll)
+                     this.listBox1.TopIndex = this.listBox1.Items.Count - (int)(this.listBox1.Height / this.listBox1.ItemHeight);
+                }
+
+                stream.Close();
+
+                //完成
+                MessageBox.Show("所有文件md5加密完成", "md5加密");
+            }
+        }
+
+        private void GetFileList(string strPath)
+        {
+            if (string.IsNullOrEmpty(strPath))
+                return;
+
+            //递归获取文件列表
+            string[] Files = Directory.GetFiles(strPath);
+
+            string[] dir = Directory.GetDirectories(strPath);
+
+            list.AddRange(Files);
+
+            for (int i = 0; i < dir.Length; ++i)
+            {
+                GetFileList(dir[i]);
+            }
+        }
+
+        private string GetFileMd5(string strFile)
+        {
+            //md5
+            MD5 md5 = new MD5CryptoServiceProvider();
+            //MD5 md5 = new MD5();
+            //文件加密
+            FileStream stream = new FileStream(strFile, FileMode.Open);
+
+            byte[] result1 = md5.ComputeHash(stream);//文件字符串
+            StringBuilder builder1 = new StringBuilder();
+            for (int i = 0; i < result1.Length; ++i)
+            {
+                builder1.Append(result1[i].ToString("x2"));
+            }
+
+            stream.Close();
+            
+            return builder1.ToString();
+        }
+    }
+}
